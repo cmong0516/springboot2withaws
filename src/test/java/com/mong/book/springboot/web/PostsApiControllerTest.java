03769c71665a4a9c43887071e48ce9da498bf7b9
cmong0516 <cmong0516@gmail.com>
2022-10-20 23:30:18 +0900

    postForEntity

--- src/test/java/com/mong/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/mong/book/springboot/web/PostsApiControllerTest.java
@@ -49,6 +49,9 @@ public class PostsApiControllerTest {
         //when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
+//        System.out.println("responseEntity = " + responseEntity);
+//        responseEntity = <200,1,[Content-Type:"application/json;charset=UTF-8", Transfer-Encoding:"chunked", Date:"Thu, 20 Oct 2022 14:29:07 GMT"]>
+
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
@@ -59,3 +62,32 @@ public class PostsApiControllerTest {
 
     }
 }
+
+// HelloController 에서 사용했던 @WebMvcTest 를 사용하지 않았다.
+// - @WebMvcTest 의 경우 JPA 기능이 작동하지 않기 때문.
+// JPA 테스트를 할땐 @SpringBootTest 와 TestRestTemplate 를 사용하자.
+
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 이거뭐임?
+// - 테스트의 웹 한경을 설정하는 속성이며 기본값은 SpringBootTest.WebEnvironment.Mock 이다.
+// Mock 은 실제 서블릿 컨테이너를 띄우지 않고 서블릿 컨테이너를 Mocking 한것이 실행된다
+// 여기선 WebEnvironment.RANDOM_PORT 를 사용하면 실제 컨테이너를 호출하며 @LocalServerPort 가 붙은
+// 변수에 값을 주입하여 실행한다.
+
+
+// @LocalServerPort 가 뭐임 ?
+// -webEnvironment.RANDOM_PORT 설정시 @LocalServerPort 가 붙은 변수에 값을 주입한다.
+
+// 고로
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 쓰면
+// @LocalServerPost 가 값을 주입받아 port 변수가 랜덤하게 생성된다.
+
+
+// private TestRestTemplate restTemplate; 가 뭐임 ?
+// - HTTP Client Rest Api 호출을 위한 함수를 제공하는 클래스.
+
+// postForEntity 가 뭐임 ?
+// - 	public <T> ResponseEntity<T> postForEntity(String url, Object request, Class<T> responseType,
+//			Object... urlVariables) throws RestClientException {
+//		return this.restTemplate.postForEntity(url, request, responseType, urlVariables);
+//	}
+// - url , requestObject , ResponseType 을 매개변수로 받아 ResponseEntity 를 반환해주네 ?
